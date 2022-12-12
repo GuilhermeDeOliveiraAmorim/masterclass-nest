8d115397338dc61b2f5b24df14de735932537633
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-12-12 12:59:28 -0300

    feat: configure prisma and created model

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './database/prisma.service';
 
 @Controller('app')
 export class AppController {
+  constructor(private prisma: PrismaService) {}
+
   @Get('hello')
-  getHello(): string {
-    return 'Opa!';
+  async getHello() {
+    const member = await this.prisma.rocketTeamMember.create({
+      data: {
+        id: 'capitao',
+        name: 'Guilherme Capitao',
+        function: 'Defesa',
+      },
+    });
+    return { member };
   }
 }
