11402fc57c9f647a8bc65f3a0599704afc4cb5ab
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-12-12 13:07:30 -0300

    feat: added DTO and class validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
+import { randomUUID } from 'crypto';
+import { CreateTeamMemberBody } from './database/dtos/create-team-member';
 
 @Controller('app')
 export class AppController {
   constructor(private prisma: PrismaService) {}
 
   @Get('hello')
-  async getHello() {
+  async getHello(@Body() body: CreateTeamMemberBody) {
+    const { name, function: memberFunction } = body;
+
     const member = await this.prisma.rocketTeamMember.create({
       data: {
-        id: 'capitao',
+        id: randomUUID(),
         name: 'Guilherme Capitao',
         function: 'Defesa',
       },
